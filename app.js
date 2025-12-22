601a137193e7b5952378ca50b541ebb33f128c21
lilbylil123 <ragebooster1998@gmail.com>
2025-12-21 20:05:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const CSV_URL =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vT6xm990HH7LTwD7X8YDM8oeG35kSGPNv0ZKEohbCdm9oDHzC77_v73RVR8KHWRa5udSKHb9oyqEc4o/pub?gid=613857331&single=true&output=csv"
   );
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyi1Tj3mFfZmWqirG_FpNPyUr3sRTGVmz8Gn7FWTkIDrcf3xyk5wPx-kotChE22X_C2WQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxT_lef4GTE6gdvqUraF11c06XVtYrFLaj4xb8Aj_xXrlXfPxAvlVyiyL6ruY0yTExFyg/exec";
 const GOLD = "#ffcc00";
 
 // Cached inventory for order modal (not affected by filters)
